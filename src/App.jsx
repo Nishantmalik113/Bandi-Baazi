94ceb13dbc437075587f3ecef87397362e1377a3
Nishantmalik113 <nishantmalik113@gmail.com>
2025-02-09 18:14:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,6 @@ import Modal from './components/modal'
 
 function App() {
   const [page, setPage] = useState('Hero')
-  const [email, setEmail] = useState(null)
-  const [password, setPassword] = useState(null)
-
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
-
-
-  function handleIncorrect(){
-    let res = document.getElementById('result')
-    res.innerText = "Incorrect Email/Password !!!"
-  }
 
   const footer =(
     <div className='footer grid grid-cols-5 gap-1 md:gap-50  md:px-20 justify-between'>
@@ -63,8 +53,7 @@ function App() {
   )
 
   return (
-    <>
-      {isAuthenticated &&(<section className='app flex flex-col justify-between'>
+      <section className='app flex flex-col justify-between'>
         {(page === 'Hero')&&(<Hero />)}
         {(page === 'Games')&&(<Games />)}
         {(page === 'Gift')&&(<Gift />)}
@@ -72,23 +61,7 @@ function App() {
         {(page === 'PhotoGallery')&&(<PhotoGallery />)}
         {(page === 'Places')&&(<Places />)}
         {footer}
-      </section>)}
-      {!isAuthenticated && (
-        <Modal>
-          <div className='flex flex-col gap-3 bg-white p-10'>
-          <h2 className='pt-0 text-xl font-semibold'>Login</h2>
-          <p>Login to your account</p>
-          <input name='Email' className='border-black border-1 rounded-lg p-2' placeholder='Email' onChange={(e)=>{setEmail(e.target.value)}} />
-          <input name='Password' className='border-black border-1 rounded-lg p-2' type="password" placeholder='**********' onChange={(e)=>{setPassword(e.target.value)}} />
-          <button onClick={()=>{
-            (email==='greatananyapandey@gmail.com') && (password==='ILoveNishant') ? (setIsAuthenticated(true)) : (handleIncorrect())
-          }}
-          className='my-3  text-sm border-2 border-black text-black font-light px-5 rounded py-1 w-fit mx-auto hover:text-white hover:bg-black'>Submit</button>
-          <p id='result' className='text-[red]'></p>
-      </div>
-        </Modal>
-      )}
-    </>
+      </section>
   )
 }
 
