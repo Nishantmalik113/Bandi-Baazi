3ec5c523695e6e6272af0e4334e3868bd72c2a5d
Nishantmalik113 <nishantmalik113@gmail.com>
2025-02-10 21:49:26 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
 
 import Hero from './components/Hero'
-
 import Gift from './components/Gift'
 import Songs from './components/Songs'
 import PhotoGallery from './components/PhotoGallery'
 import Places from './components/Places'
 import Games from './components/Games'
+import Modal from './components/Modal'
 
 
 function App() {
   const [page, setPage] = useState('Hero')
+  const [email, setEmail] = useState(null)
+  const [password, setPassword] = useState(null)
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [incorrect, setIncorrect] = useState(false)
 
   const footer =(
     <div className='footer grid grid-cols-5 gap-1 md:gap-50  md:px-20 justify-between'>
@@ -53,7 +57,8 @@ function App() {
   )
 
   return (
-      <section className='app flex flex-col justify-between'>
+    <>
+      {isAuthenticated && (<section className='app flex flex-col justify-between'>
         {(page === 'Hero')&&(<Hero />)}
         {(page === 'Games')&&(<Games />)}
         {(page === 'Gift')&&(<Gift />)}
@@ -61,7 +66,25 @@ function App() {
         {(page === 'PhotoGallery')&&(<PhotoGallery />)}
         {(page === 'Places')&&(<Places />)}
         {footer}
-      </section>
+      </section>)}
+      {!isAuthenticated&&(
+        <Modal>
+          <div className='bg-gradient-to-t from-white to-pink-200 flex flex-col gap-5 justify-center p-10'>
+            <h1 className='heading'>Login</h1>
+            <p>Login to your account: </p>
+            <input id='Email' placeholder='Email' onChange={(e)=>{setEmail(e.target.value)}} className='p-2 border-2 border-black rounded-lg'/>
+            <input id='Password' type="password" placeholder='**********' onChange={(e)=>{setPassword(e.target.value)}} className='p-2 border-2 border-black rounded-lg'/>
+            <button onClick={()=>{
+              (email==="greatananyapandey@gmail.com" && password==="ILoveNishant") ? (setIsAuthenticated(true)) :(setIncorrect(true))
+            }}
+            className='my-3  text-sm border-2 border-black font-light text-black px-5 rounded py-1 w-fit mx-auto hover:text-white hover:bg-black hoevr:transition duration-1000'>
+              Submit
+            </button>
+            {incorrect&&(<p className='font-semibold text-red-600'>Incorrect Email/Password!!!</p>)}
+          </div>
+        </Modal>
+      )}
+    </>
   )
 }
 
